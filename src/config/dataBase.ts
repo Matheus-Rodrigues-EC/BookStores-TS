b8c9064da847f7fc549e2e00365affa8758d3a3c
Matt Rodrigues <68609506+Matt-Rodrigues-EC@users.noreply.github.com>
2023-07-01 15:08:11 -0300

    Fix database
    Reconfigured DataBase to run with .env

--- src/config/dataBase.ts
+++ src/config/dataBase.ts
@@ -1,13 +1,23 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config()
 
 const { Pool } = pg;
 
+// const connection = new Pool({
+//     host: process.env.HOST,
+//     port: Number(process.env.DB_PORT),
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+// });
+
+// export { connection };~
+
 const connection = new Pool({
-    host:       'localhost',
-    port:       5432,
-    user:       'postgres',
-    password:   '06021998',
-    database:   'BookStore'
-});
+    connectionString: process.env.DATABASE_URL
+})
+
+console.log('DataBase is Connected...');
 
 export { connection };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-const port = 5000;
 const app = express();
 app.get('/health', (req: Request, res: Response) => {
     res.status(200).send("I'm Alive")
@@ -12,4 +13,4 @@ app.use(book);
 
 // app
 
-app.listen(port, () => console.log("Server is running at port: " + port));
+app.listen(process.env.PORT, () => console.log("Server is running at port: " + process.env.PORT));
